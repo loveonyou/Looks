feb123668aba9d2959eec06705639ac9ed511c68
Adn <nimouhq@gmail.com>
2023-11-02 23:49:13 +0100

    Update looks.py

--- looks.py
+++ looks.py
@@ -2,8 +2,9 @@ import os, argparse, asyncio
 from fonction.app import *
 from colorama import *
 
+
 __author__ = "adn"
-__version__ = "0.1"
+__version__ = "0.3"
 
 if __name__ == "__main__":
     init()
@@ -14,9 +15,13 @@ if __name__ == "__main__":
 
     )
     parser.add_argument('-u', dest= "username", help='user peusdo you want to found', required=True)
+    parser.add_argument('-e', dest= "email", help='user peusdo you want to found', required=False)
+    parser.add_argument('-n', dest= "number", help='user peusdo you want to found', required=False)
+    
     parser.add_argument('-v',  action='version', version=f'%(prog)s {__version__}')
     args = parser.parse_args()
     username = args.username
+    email = args.email
 
     os.system(f"title LookTools user {os.getlogin()}")  
 
