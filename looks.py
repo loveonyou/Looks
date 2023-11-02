365e425c6cf8650d883f1f8ea2f28ea3be7fe33b
Adn <nimouhq@gmail.com>
2023-11-02 20:57:48 +0100

    Update looks.py

--- looks.py
+++ looks.py
@@ -1,8 +1,12 @@
 import os, argparse
+from fonction.app import *
+from colorama import *
+
 __author__ = "adn"
 __version__ = "0.1"
 
 if __name__ == "__main__":
+    init()
     parser = argparse.ArgumentParser(
         prog= 'LookTools',
         description= 'Osint tools for look up peusdo and get all information available on the network.',
@@ -12,7 +16,9 @@ if __name__ == "__main__":
     parser.add_argument('-u', dest= "username", help='user peusdo you want to found', required=True)
     parser.add_argument('-v',  action='version', version=f'%(prog)s {__version__}')
     args = parser.parse_args()
+    username = args.username
 
-    os.system(f"title LookTools user {os.getlogin()}")
+    os.system(f"title LookTools user {os.getlogin()}")  
 
-    input()
+    
+    
